865708c06a57c3055402663547cb8a13a8e39ecd
nguenhien153 <42664186+nguenhien153@users.noreply.github.com>
2019-01-15 02:37:16 +0700

    new project

---
+++ ShoppingJewellery/ShoppingJewellery/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingJewellery.Models;
+namespace ShoppingJewellery.Controllers
+{
+    public class HomeController : Controller
+    {
+        JewelleryShopping_dbEntities db = new JewelleryShopping_dbEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.ViewDisplayItems);
+        }
+    }
+}
---
+++ ShoppingJewellery/ShoppingJewellery/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingJewellery.Models;
+namespace ShoppingJewellery.Controllers
+{
+    public class ProductController : Controller
+    {
+        JewelleryShopping_dbEntities db = new JewelleryShopping_dbEntities();
+        // GET: Product
+        public ActionResult Product(string Style_Code)
+        {
+            var item = db.ViewFullItems.First(itm=>itm.Style_Code.Contains(Style_Code));
+            if(item==null)
+            {
+                return RedirectToAction("Index", "Home");
+            }else
+            {
+                return View(item);
+            }
+           
+        }
+    }
+}
